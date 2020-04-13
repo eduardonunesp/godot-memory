4a0363b78577feac262f662f4c54854e1d844891
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2020-04-13 11:23:30 -0300

    Cards on 2d grid

--- Card.gd
+++ Card.gd
@@ -1,6 +1,7 @@
 extends Area2D
 class_name Card
 
+onready var label = get_node("Label")
 onready var face_up = get_node("FaceUp")
 onready var face_down = get_node("FaceDown")
 
@@ -14,9 +15,17 @@ export (CardState) var state = CardState.FACE_DOWN
 func _process(delta: float):
 	match state:
 		CardState.FACE_DOWN:
-			face_up.visible = false
+			_on_face_down()
 		CardState.FACE_UP:
-			face_up.visible = true
+			_on_face_up()
+
+func _on_face_down():
+	face_up.visible = false
+	label.text = "FACE DOWN"
+
+func _on_face_up():
+	face_up.visible = true
+	label.text = "FACE UP"
 
 func _on_card_clicked(viewport: Node, event: InputEvent, shape_idx: int):
 	if (event is InputEventMouseButton && event.pressed):
--- World.gd
+++ World.gd
@@ -1,5 +1,23 @@
 extends Node
 
+onready var card = preload("res://Card.tscn")
+onready var start_position = get_node("StartPosition")
+
+export var grid_x_size = 5
+export var grid_y_size = 5
+export var grid_x_space = 100
+export var grid_y_space = 100
+
+var cards = [[]]
+
+func _ready():
+	for x in range(grid_x_size):
+		for y in range(grid_y_size):
+			var new_card: Card = card.instance()
+			new_card.position.x = start_position.position.x + grid_x_space * x
+			new_card.position.y = start_position.position.y + grid_y_space * y
+			add_child(new_card)
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed('ui_cancel'):
 		get_tree().quit(0)
