ffa226ea20e648b914b53419c97b963fcdbf0040
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2020-04-13 12:15:29 -0300

    Adding random cards numbers

--- Card.gd
+++ Card.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name Card
 
 onready var label = get_node("Label")
+onready var number = get_node("Number")
 onready var face_up = get_node("FaceUp")
 onready var face_down = get_node("FaceDown")
 
@@ -12,6 +13,8 @@ enum CardState {
 
 export (CardState) var state = CardState.FACE_DOWN
 
+var card_number = 0
+
 func _process(delta: float):
 	match state:
 		CardState.FACE_DOWN:
@@ -19,12 +22,16 @@ func _process(delta: float):
 		CardState.FACE_UP:
 			_on_face_up()
 
+	number.text = str(card_number)
+
 func _on_face_down():
 	face_up.visible = false
+	number.visible = false
 	label.text = "FACE DOWN"
 
 func _on_face_up():
 	face_up.visible = true
+	number.visible = true
 	label.text = "FACE UP"
 
 func _on_card_clicked(viewport: Node, event: InputEvent, shape_idx: int):
--- World.gd
+++ World.gd
@@ -8,15 +8,32 @@ export var grid_y_size = 5
 export var grid_x_space = 100
 export var grid_y_space = 100
 
-var cards = [[]]
+var cards = []
 
 func _ready():
-	for x in range(grid_x_size):
-		for y in range(grid_y_size):
+	var random_numbers = _get_random_numbers()
+	var cards = _create_cards(random_numbers)
+	for card in cards:
+		add_child(card)
+
+func _create_cards(numbers):
+	var new_cards = []
+	for y in range(grid_x_size):
+		for x in range(grid_y_size):
 			var new_card: Card = card.instance()
 			new_card.position.x = start_position.position.x + grid_x_space * x
 			new_card.position.y = start_position.position.y + grid_y_space * y
-			add_child(new_card)
+			new_card.card_number = numbers.pop_front()
+			new_cards.push_back(new_card)
+	return new_cards
+
+func _get_random_numbers():
+	var numbers = []
+	for i in range(1, (grid_x_size * grid_y_size) + 1):
+		numbers.push_back(i)
+	randomize()
+	numbers.shuffle()
+	return numbers
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed('ui_cancel'):
